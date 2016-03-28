7fdca765dee601379f2860ecfaf118f232fe219f
Oasis14 <hkyryan14@gmail.com>
2016-03-28 19:21:19 +0800

    Added files via upload

--- PlayerSkeleton.java
+++ PlayerSkeleton.java
@@ -3,7 +3,14 @@ public class PlayerSkeleton {
 
 	//implement this function to have a working system
 	public int pickMove(State s, int[][] legalMoves) {
-		
+		//Hight -- Ryan 
+		//Lines completed -- James
+		//Holes -- Michael
+		//Bumping -- Andres
+
+		// Go through all the elements in legal move
+		// Send them each to the huerestics(hight, lines completed, holes, ect)
+		// Keep track of what is the best option and make that move
 		return 0;
 	}
 	
