238944dab4faa35a716aa41bf983df70f6acbd9b
Michael Novén <michael.f.noven@gmail.com>
2016-03-30 01:48:20 +0800

    iterate through legal moves and add function skeletons we need to implement

--- PlayerSkeleton.java
+++ PlayerSkeleton.java
@@ -1,17 +1,65 @@
 
+import java.util.*;
+
 public class PlayerSkeleton {
+
+
+	/** We need to go through the field and fill it with new 1's at the 
+	    positions where the piece was dropped, then return the new field */
+	public int[][] dropPiece(int orient, int slot, int piece, int[][] currField){
+
+		return currField;
+
+	}
+
+	/** Calculate the new fields score based on our awesome 
+		heuristics */
+	public int calcScore(int[][] newField){
+
+		// Our magic numbers :)
+		double a = -0.510066, b = 0.760666, c = -0.35663, d = -0.184473;
+		int score = 0;
+
+		//int height = calcHeight(newField); - Ryan
+		//int linesCompleted = calcLinesCompleted(newField) -- James
+		//int numHoles = calcNumHoles(newField) -- Michael
+		//int bumpiness = calcBumpiness(newField) -- Andres
+
+		// Multiply weights with scores and add together...
+		return score;
+
+	}
+
 
 	//implement this function to have a working system
 	public int pickMove(State s, int[][] legalMoves) {
-		//Hight -- Ryan 
-		//Lines completed -- James
-		//Holes -- Michael
-		//Bumping -- Andres
 
-		// Go through all the elements in legal move
-		// Send them each to the huerestics(hight, lines completed, holes, ect)
-		// Keep track of what is the best option and make that move
-		return 0;
+		int orient = 0, slot = 0, rowCounter = 0,piece;
+		TreeMap<Integer, Integer> scores = new TreeMap<Integer, Integer>();
+
+		//System.out.println("** Piece " + s.getNextPiece());
+
+		// Go through every possible move
+		for(int i = 0; i < legalMoves.length; i++){
+
+			for(int j = 0; j < legalMoves[i].length; j++){
+
+				orient = legalMoves[i][j];
+				slot = legalMoves[i][j];
+
+			}
+
+			// Get the representation of the field when the piece is dropped and collisions calculated
+			int[][] newField = dropPiece(orient, slot, s.getNextPiece(), s.getField());
+			// How well does this state perform?
+			int score = calcScore(newField);
+			// We map this score to the current move (which is defined by row index)
+			scores.put(score, rowCounter++);
+
+		}
+
+		return scores.firstKey(); // We want the move associated to the highest score
+	
 	}
 	
 	public static void main(String[] args) {
