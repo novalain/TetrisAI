69cc24e3771417d384c28d1ff991c5eb85594171
Michael Novén <michael.f.noven@gmail.com>
2016-04-12 22:50:34 +0800

    fix when highest scores are equal - select random one instead of first one

--- PlayerSkeleton.java
+++ PlayerSkeleton.java
@@ -3,7 +3,6 @@ import java.util.*;
 import java.io.*;
 import java.util.Random;
 
-
 public class PlayerSkeleton {
 	private static Random randnum;
 
@@ -72,6 +71,9 @@ public class PlayerSkeleton {
 		double completeLines = 0;
 		double numHoles = 0;
 		double bumpiness = 0;
+
+
+		double bumpinessNew = 0;
 		
 		int lowestTop = 100;
 
@@ -83,11 +85,12 @@ public class PlayerSkeleton {
 				bumpiness += Math.abs(newTop[col]-newTop[col-1]);
 			}	
 		}
+
 		// Calculate Height and Bumpiness
 		for (int j = 0; j < maxCol; j++){
 			// Boolean representation of if we have found the columns top yet
 			// Do we need to check top row? If we're there we lost right?
-			for (int i = newTop[j]; i >= 0; i--) {
+			for (int i = newTop[j]-1; i >= 0; i--) {
 				// If the top filled square of the column is not yet found and we found a filled square 
 				if(newField[i][j] == 0) {
 					//It must be a hole, so increment
@@ -130,6 +133,83 @@ public class PlayerSkeleton {
 
 	}
 
+	public double bruteForceFitness(final int[][] newField){
+
+		double a = -0.510066, b = 0.760666, c = -0.35663, d = -0.184473;
+		int score = 0;
+
+		// calc height
+		int height = 0;
+		int maxRow = newField.length;
+		int maxCol = newField[0].length;
+		
+		// Height
+		for(int col = maxCol - 1; col >= 0; col--){
+			for(int row = maxRow - 1; row >=0; row--){
+				if(newField[row][col] != 0){
+					//System.out.println("row, col "+ row +" "+ col);
+					height += (row + 1);
+					break;
+				}
+			}
+		}
+
+		// Lines cleared 
+		int linesCleared = 0;
+		for(int i = 0; i < State.ROWS; i++ ){
+			boolean isCompleted = true;
+			for(int j = 0; j < State.COLS; j++){
+				if(newField[i][j] == 0)
+					isCompleted = false;
+			}
+			if(isCompleted)
+				linesCleared++;
+		}
+
+
+		// Holes
+		int holes = 0;
+		for (int j = 0; j < State.COLS; j++){
+			// Boolean representation of if we have found the columns top yet
+			int top = 0;
+			// Do we need to check top row? If we're there we lost right?
+			for (int i = State.ROWS - 1; i >= 0; i--) {
+				// If the top filled square of the column is not yet found and we found a filled square 
+				if (newField[i][j] != 0 && top == 0) {
+					// Set top to found
+					top = 1;
+				}
+				// If the column's top has been found and we have found an empty square
+				if (top == 1 && newField[i][j] == 0) {
+					//It must be a hole, so increment
+					holes += 1;
+				}
+			}
+		}
+
+		// Bumbiness
+		int bumpiness = 0;
+		int lRowHeight = 0, rRowHeight = 0;
+		
+		for(int i = 0; i < State.COLS; i++ ){
+			for(int j = State.ROWS-1; j >= 0; j--){
+				if(newField[j][i] != 0 || j == 0) {
+					rRowHeight = j+(newField[j][i] != 0 ? 1:0);
+					if(i!= 0) {
+						bumpiness += Math.abs(lRowHeight-rRowHeight);
+					}
+					lRowHeight = rRowHeight;
+					// System.out.printf("%d, ", rRowHeight);
+					break;
+				}
+			}
+		}
+
+		return height*a + linesCleared*b + holes*c + bumpiness*d;
+		
+  	}
+
+
 	/**
 	 * Method that obtains the best moe by iterating through the list of legalMoves and calculating the 
 	 * best score based on a fitness function
@@ -140,6 +220,9 @@ public class PlayerSkeleton {
 	 */
 	public int pickMove(State s, int[][] legalMoves, double[] weightFactors) {
 
+		//TreeMap<Double, Integer> scores = new TreeMap<Double, Integer>();
+		//Map<Double, List<Integer>> scores = new Map<Double, int[]>(); 
+		ArrayList<Integer> possibleMoves = new ArrayList<Integer>();
 
 		int orient, slot;
 		// TreeMap<Double, Integer> scores = new TreeMap<Double, Integer>();
@@ -168,23 +251,29 @@ public class PlayerSkeleton {
 
 			// Get the representation of the field when the piece is dropped and collisions calculated (newField will change and passed as a pointer)
 			if(makeTheoreticalMove(orient, slot, newField, pWidth, pHeight, pTop, pBottom, newTop, nextPiece, turnNumber)){
-				
 				// How well does this state perform?
 				double score = fitnessFunction(newField, newTop, weightFactors);
-				// We map this score to the current move (which is defined by row index)
-				
-				// scores.put(score, i);
-				
-				// System.out.printf("%f ", score);
-				if(score > highestScore){
-					moveKey = i;
+				//assert score != brute_force_score;
+
+				/*if(Math.abs(score - brute_force_score) > 0.000000001)
+					System.out.println("** DIFF IN BRUTE AND FITNESS");*/
+					
+				// Equal to highest
+				if(Math.abs(score - highestScore) < 0.000000001){
+					//System.out.println("Warning, scores are equal!");
+					possibleMoves.add(i);
+				}
+
+				// New highest score
+				else if(score > highestScore){
+					possibleMoves.clear();
+					possibleMoves.add(i);
 					highestScore = score;
 				}
 			}
-
 		}
 
-		return moveKey; // We want the move associated to the highest score
+		return possibleMoves.size() == 0 ? 0 : possibleMoves.get(randnum.nextInt(possibleMoves.size()));
 	
 	}
 	/**
@@ -308,15 +397,19 @@ public class PlayerSkeleton {
 	 *
 	 * @param args If an argument is passed(Whatever it is) it will run the evolution, otherwise it will run the game
 	 */
+
+
 	public static void main(String[] args) {
 		randnum = new Random();
 		double weightFactors[] = {-0.510066, 0.760666, -0.35663, -0.184473};
-		if(args.length > 0){
+		
+		if(args.length > 1){
 			weightFactors = RunEvolution(4);
 			// Returns empty, not yet finished
 			System.out.println("Completed evolution with: " + weightFactors[0] + " " + weightFactors[1] + " " + weightFactors[2] + " " + weightFactors[3]);
 		}
-		System.out.println("You have completed "+ RunAI(weightFactors, 300, Integer.MAX_VALUE)+" rows.");
+		System.out.println("You have completed "+ RunAI(weightFactors, 20, Integer.MAX_VALUE)+" rows.");
 	}
 	
 }
+
